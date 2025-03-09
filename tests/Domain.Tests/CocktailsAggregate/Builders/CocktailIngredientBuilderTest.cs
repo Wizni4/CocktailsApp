a06b77772e5b12950d9f73dc658b0a7c7cf3cf3b
Mathieu Masson <mathieumasson555@hotmail.com>
2025-03-09 03:00:20 +0100

    setup unit tests

---
+++ tests/Domain.Tests/CocktailsAggregate/Builders/CocktailIngredientBuilderTest.cs
@@ -0,0 +1,40 @@
+﻿using FluentAssertions;
+using Domain.CocktailAggregate;
+
+namespace Domain.Tests.CocktailsAggregate
+{
+    public class CocktailIngredientBuilderTest
+    {
+        [Test]
+        public void AddName_WithValidName_ReturnCocktail()
+        {
+
+            // Arrange
+            var name = "Mojito"; // Valid name to set
+
+            // Act
+            var builder = new CocktailBuilder().AddName(name);
+
+            // Assert
+            // Since the CocktailBuilder has no getter for the _name, 
+            // we need to check if the internal state has been updated correctly.
+            // We can use reflection to access private fields for testing.
+            var nameField = typeof(CocktailBuilder)
+                .GetField("_name", System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Instance);
+            var actualName = nameField.GetValue(builder);
+
+            actualName.Should().Be(name);
+        }
+
+        [Test]
+        public void AddName_WithEmptyName_ThrowsArgumentException()
+        {
+            // Arrange
+            var emptyName = ""; // Empty name input
+
+            // Act & Assert
+            Action act = () => new CocktailBuilder().AddName(emptyName);
+            act.Should().Throw<ArgumentException>().WithMessage("Name cannot be empty");
+        }
+    }
+}
