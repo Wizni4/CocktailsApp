fcd0d128cdaadadc2c41eff0aac9af56cdfc5f27
Mathieu Masson <mathieumasson555@hotmail.com>
2025-03-09 03:24:02 +0100

    test that

--- tests/Domain.Tests/CocktailsAggregate/Builders/CocktailIngredientBuilderTest.cs
+++ tests/Domain.Tests/CocktailsAggregate/Builders/CocktailIngredientBuilderTest.cs
@@ -1,5 +1,6 @@
-﻿using FluentAssertions;
-using Domain.CocktailAggregate;
+﻿using Domain.CocktailAggregate;
+
+using FluentAssertions;
 
 namespace Domain.Tests.CocktailsAggregate
 {
@@ -21,7 +22,7 @@ namespace Domain.Tests.CocktailsAggregate
             // We can use reflection to access private fields for testing.
             var nameField = typeof(CocktailBuilder)
                 .GetField("_name", System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Instance);
-            var actualName = nameField?.GetValue(builder);
+            var actualName = nameField.GetValue(builder);
 
             actualName.Should().Be(Name);
         }
