768effbf4c41449f84611a71e7a9304660ace940
Mathieu Masson <mathieumasson555@hotmail.com>
2025-03-09 23:35:54 +0100

    test (should fail)

--- src/Domain/ClubAggregate/Entities/ClubMember.cs
+++ src/Domain/ClubAggregate/Entities/ClubMember.cs
@@ -20,7 +20,7 @@ namespace CocktailsApp.Domain.ClubAggregate
             UserId = userId;
         }
 
-        internal void AddPermission(ClubAction action)
+        public void AddPermission(ClubAction action)
         {
             // Ensure the member doesn't already have this permission.
             if (_permissions.Any(p => p.Action == action))
