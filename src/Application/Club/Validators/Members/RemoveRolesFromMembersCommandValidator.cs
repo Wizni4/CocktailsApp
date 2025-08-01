ac93a6605e4a54dbeb54d9940afcf1bc3135490d
Mathieu Masson <mathieu.masson@alten.com>
2025-08-01 05:05:28 +0200

    allez là

--- src/Application/Club/Validators/Members/RemoveRolesFromMembersCommandValidator.cs
+++ src/Application/Club/Validators/Members/RemoveRolesFromMembersCommandValidator.cs
@@ -10,6 +10,7 @@
  * Framework namespaces
  */
 using CocktailsApp.Application.SeedWork;
+using CocktailsApp.Domain.ClubAggregate;
 
 using FluentValidation;
 
@@ -18,18 +19,61 @@ namespace CocktailsApp.Application.Club
     /// <summary>
     /// Validates the <see cref="RemoveRolesFromMembersCommand"/> to ensure all required identifiers are provided and valid.
     /// </summary>
-    public class RemoveRolesFromMembersCommandValidator : AbstractValidator<RemoveRolesFromMembersCommand>
+    public class RemoveRolesFromMembersCommandValidator : ClubBaseValidator<RemoveRolesFromMembersCommand>
     {
         /// <summary>
         /// Initializes a new instance of the <see cref="RemoveRolesFromMembersCommandValidator"/> class.
         /// Defines validation rules for the <see cref="RemoveRolesFromMembersCommand"/>.
         /// </summary>
-        public RemoveRolesFromMembersCommandValidator()
+        public RemoveRolesFromMembersCommandValidator(IClubRepository clubRepository)
+            : base(clubRepository, [ClubPermissionType.RemoveRoleFromMember])
         {
-            RuleFor(c => c.ClubId).ValidGuid();
             RuleFor(c => c.Members).ValidList();
             RuleForEach(c => c.Members).SetValidator(new MemberRolesUpdateModelValidator());
-            RuleFor(c => c.ActorId).ValidGuid();
+            RuleFor(c => c)
+                .CustomAsync(async (command, context, _) =>
+                {
+                    var club = await clubRepository.GetClubBydIdAsync(
+                        command.ClubId,
+                        opt => opt.Include(c => c.Roles)
+                                   .Include(c => c.Members)
+                                    .ThenInclude(c => c.Roles));
+
+                    if (club != null)
+                    {
+                        AssertMissingEntities(
+                            context,
+                            club.Members,
+                            command.Members.Select(m => m.Id));
+
+                        AssertMissingEntities(
+                            context,
+                            club.Roles,
+                            command.Members.SelectMany(m => m.RoleIds));
+
+                        // Get the list of found members
+                        var foundMembers = club.Members
+                            .Where(cm => command.Members
+                            .Any(m => new ClubMemberByIdSpecification(m.Id).SpecExpression.Compile()(cm)));
+
+                        // Check if the members doesnt have the specified roles
+                        foreach (var clubMember in foundMembers)
+                        {
+                            var requestedRoles = command.Members
+                                .FirstOrDefault(m => m.Id == clubMember.Id)?.RoleIds;
+
+                            var notExistingRoles = (requestedRoles ?? Enumerable.Empty<Guid>())
+                                .Where(id => !clubMember.Roles
+                                .Any(r => new ClubRoleByIdSpecification(id).SpecExpression.Compile()(r)));
+
+                            if (notExistingRoles.Any())
+                                context.AddFailure(
+                                    $"The member: '{clubMember.Id}' does not have the following roles:\n- {string.Join("\n- ", notExistingRoles)}");
+
+                        }
+                    }
+                        
+                });
         }
     }
 }
