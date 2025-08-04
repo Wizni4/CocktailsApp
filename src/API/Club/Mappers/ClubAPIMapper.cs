0fcf30402169099c983afbc638ea866fea919605
Mathieu Masson <mathieu.masson@alten.com>
2025-08-04 03:07:33 +0200

    trf

--- src/API/Club/Mappers/ClubAPIMapper.cs
+++ src/API/Club/Mappers/ClubAPIMapper.cs
@@ -3,6 +3,7 @@
 // See the LICENSE file in the project root for more information.
 
 using AutoMapper;
+
 using CocktailsApp.API.Extensions;
 using CocktailsApp.Application.Club;
 using CocktailsApp.Domain.ClubAggregate;
